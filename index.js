02798fdb50ace8e523c04eb9a9b3377b75ad7ad6
PK <pushpendra.bhumca2013@gmail.com>
2021-10-12 04:29:19 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const add = (a,b) => a+b;
+const subtract = (a,b) => a-b
+const mul = (a,b) => a*b
+const divison = (a,b) => a/b
+
+module.exports = {
+    add,
+    subtract,
+    mul,
+    divison
+}
